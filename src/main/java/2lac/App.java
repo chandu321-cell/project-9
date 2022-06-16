d25ea5a9fa1551cbc8a2de55275ec3d7d5ab432b
chandu321-cell <chandrakanth@gmail.com>
2022-06-16 16:20:13 +0530

    code is moves from repo

---
+++ src/main/java/2lac/App.java
@@ -0,0 +1,13 @@
+package 2lac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "real time project!" );
+    }
+}
